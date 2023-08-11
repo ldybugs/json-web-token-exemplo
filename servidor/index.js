d598621f9c6199c495f1bbe7fcffd3dcd867ff5b
ldybugs <vitoriaoliveiramedeiros2006@gmail.com>
2023-08-11 17:13:50 -0400

    commit update

--- servidor/index.js
+++ servidor/index.js
@@ -25,7 +25,7 @@ app.use(
     secret: process.env.SECRET,
     algorithms: ["HS256"],
     getToken: req => req.cookies.token
-  }).unless({ path: ["/autenticar", "/logar", "/deslogar", "/"] })
+  }).unless({ path: ["/autenticar", "/logar", "/deslogar"] })
 );
 
 app.get('/autenticar', async function(req, res){
@@ -38,17 +38,24 @@ app.get('/', async function(req, res){
 
 app.post('/logar', (req, res) => {
   if(req.body.usuario == 'vivi' && req.body.senha == '123') {
-    res.send ("login feito")
+    const id = 1;
+    const token = jwt.sign({ id }, process.env.SECRET, {
+      expiresIn: 300
+    })
+    res.cookie('token', token, {httpOnly:true});
+    return res.json({
+      usuario: req.body.usuario,
+      token: token
+    })
   }
-  else{
-    res.send ("login recusado")
-  }
+    res.status(500).json({mensagem: "Login errado"})
 })
 
 app.post('/deslogar', function(req, res) {
-  
+  res.cookie('token', null, {httpOnly:true});
+  res.json({deslogar:true})
 })
 
 app.listen(3001, function() {
-  console.log('App de Exemplo escutando na porta 3001!')
+  console.log('App de Exemplo escutando na porta 3000!')
 });
